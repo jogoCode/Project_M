9d633877cc82ff1a150cddf1115382369475c4f4
Frederheat <keplerfrederic@gmail.com>
2024-03-25 11:50:12 +0400

    ajout d'item + weapon attrapable au sol

---
+++ Assets/Scripts/Interfaces/IPickable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickable
+{
+    public void Pick()
+    { Debug.Log("j'ai attraper quelquechose");}
+}
